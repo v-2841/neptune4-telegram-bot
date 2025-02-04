499461863df71d14b29472e113f0fb5194da99cc
Vitaliy Pavlov <v-2841@yandex.ru>
2025-02-04 19:08:00 +0400

    04.02.25

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-import json
 import os
 
 from dotenv import load_dotenv
-from telegram import Update
+from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import (Application, ApplicationBuilder, CommandHandler,
-                          ContextTypes)
+                          ContextTypes, MessageHandler)
+from telegram.ext.filters import Regex, Text
 
 from printer import PrinterAPI
 
@@ -12,18 +12,35 @@ from printer import PrinterAPI
 load_dotenv()
 
 
+def create_main_menu() -> ReplyKeyboardMarkup:
+    """Создает главное меню."""
+    return ReplyKeyboardMarkup(
+        [["Состояние принтера"]],
+        resize_keyboard=True, is_persistent=True,
+    )
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        'Привет!', reply_markup=create_main_menu())
+
+
+async def printer_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     printer_api: PrinterAPI = context.bot_data['printer_api']
-    config = await printer_api.printer_info()
-    await update.message.reply_text(json.dumps(
-        config, sort_keys=True, indent=4))
+    result = await printer_api.printer_info()
+    await update.message.reply_text(result)
 
 
-async def post_init(application: Application) -> None:
+async def unknown_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        'Неизвестная команда', reply_markup=create_main_menu())
+
+
+async def post_init(application: Application):
     application.bot_data['printer_api'] = PrinterAPI()
 
 
-async def post_shutdown(application: Application) -> None:
+async def post_shutdown(application: Application):
     await application.bot_data['printer_api'].close()
 
 
@@ -32,4 +49,7 @@ if __name__ == "__main__":
         'TELEGRAM_BOT_TOKEN', 'token')).post_init(post_init).post_shutdown(
             post_shutdown).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(
+        Regex("^Состояние принтера$"), printer_info))
+    app.add_handler(MessageHandler(Text(), unknown_command))
     app.run_polling()
--- printer.py
+++ printer.py
@@ -9,25 +9,38 @@ class PrinterAPI:
             headers={
                 'CF-Access-Client-Id': os.getenv('CLOUDFLARE_AC_ID', 'id'),
                 'CF-Access-Client-Secret': os.getenv(
-                    'CLOUDFLARE_AC_SECRET', 'secret')
-            }
+                    'CLOUDFLARE_AC_SECRET', 'secret'),
+            },
+            raise_for_status=self.response_error,
         )
         self.printer_url = os.getenv('PRINTER_URL', 'printer_url')
 
-    async def server_config(self):
-        async with self.session.get(
-                self.printer_url + '/server/config') as response:
-            return await response.json()
-
     async def printer_info(self):
-        async with self.session.get(
-                self.printer_url + '/printer/info') as response:
-            return await response.json()
+        try:
+            async with self.session.get(
+                    self.printer_url + '/printer/info') as response:
+                data = await response.json()
+                return data['result']['state_message']
+        except Exception as e:
+            return e
 
-    async def proc_stats(self):
-        async with self.session.get(
-                self.printer_url + '/machine/proc_stats') as response:
-            return await response.json()
+    # async def proc_stats(self):
+    #     async with self.session.get(
+    #             self.printer_url + '/machine/proc_stats') as response:
+    #         return await response.json()
+
+    # async def server_config(self):
+    #     async with self.session.get(
+    #             self.printer_url + '/server/config') as response:
+    #         return await response.json()
+
+    async def response_error(self, response):
+        if response.status == 530:
+            raise RuntimeError(
+                'Статус 530: Принтер выключен или находится не в сети')
+        if not response.ok:
+            raise RuntimeError(
+                f'Статус {response.status}: {response.reason}')
 
     async def close(self):
         await self.session.close()
